95d19c5813de97557aacfd0a1d8dbfa36531bc3a
kiruthiga <kiruthiga.arasilangovan@gmail.com>
2022-07-13 20:22:08 +0530

    robocopy

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+set -e
+USER-ID=$(id -u)
+if [USER-ID -ne 0]; then
+    echo -e "/e [31m you need to run it as a root user /e[0m"
+    exit 1
+fi
 yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
