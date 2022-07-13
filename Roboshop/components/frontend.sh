bc3e1db86731b9eec016cf67681aa7b5cf9d0d4c
kiruthiga <kiruthiga.arasilangovan@gmail.com>
2022-07-13 20:41:47 +0530

    robocopy

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-USER-ID=$(id -u)
-if [USER-ID -ne 0]; then
+USER_ID=$(id -u)
+if [USER_ID -ne 0]; then
     echo -e "/e [31m you need to run it as a root user /e[0m"
     exit 1
 fi
