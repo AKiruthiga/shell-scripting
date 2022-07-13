35b24d92ca266acdfaed40a0e9ff31203924b3f9
kiruthiga <kiruthiga.arasilangovan@gmail.com>
2022-07-13 20:43:52 +0530

    robocopy

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -2,7 +2,7 @@
 set -e
 USER_ID=$(id -u)
 if [USER_ID -ne 0]; then
-    echo -e "/e [31m you need to run it as a root user /e[0m"
+    echo -e "\e [31m you need to run it as a root user \e[0m"
     exit 1
 fi
 yum install nginx -y
