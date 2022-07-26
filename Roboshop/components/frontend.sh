af7ffa75dcf0897db102eecd9f9877c65594317b
kiruthiga <kiruthiga.arasilangovan@gmail.com>
2022-07-26 14:22:38 +0530

    robocopy

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -e
-
+COMPONENT=frontend
+LOGFILE="/tmp/$COMPONENT.LOG"
 source components/common.sh
- 
-yum install nginx -y
+
+echo "installing the nginx"
+yum install nginx -y &>> $LOGFILE
+if [$? -eq 0] then
+    echo -e "\e [32m SUCCESS \e[0m"
+else
+    echo -e "\e [31m FAILURE \e[0m"
+fi
 systemctl enable nginx
 systemctl start nginx
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
