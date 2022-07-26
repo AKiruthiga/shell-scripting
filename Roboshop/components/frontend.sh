fa85d62cd9ac50c1915dd6c120a98eb8c09bce23
kiruthiga <kiruthiga.arasilangovan@gmail.com>
2022-07-26 14:41:09 +0530

    robocopy

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -5,21 +5,39 @@ LOGFILE="/tmp/$COMPONENT.log"
 
 source components/common.sh
 
-echo -n "installing the nginx"
-yum install nginx -y &>> $LOGFILE
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m SUCCESS \e[0m"
-else
-    echo -e "\e[31m FAILURE \e[0m"
-fi
-systemctl enable nginx
-systemctl start nginx
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+stat() { 
+    if [ $1 -eq 0 ] ; then
+        echo -e "\e[32m SUCCESS \e[0m"
+    else
+        echo -e "\e[31m FAILURE \e[0m"
+    fi
+}
 
+echo -n "installing the nginx:"
+yum install nginx -y &>> $LOGFILE
+stat $?
+
+systemctl enable nginx &>> $LOGFILE
+echo -n "starting nginx:"
+systemctl start nginx
+stat $?
+
+echo -n "Downloading and extracting $COMPONENT:"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+stat $?
+
+echo -n "clearing the old content:"
 cd /usr/share/nginx/html
 rm -rf *
-unzip /tmp/frontend.zip
+stat $?
+
+echo -n "extracting the downloaded content:"
+unzip /tmp/frontend.zip &>> $LOGFILE
+stat $?
+
+echo -n "updating the proxy file:"
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $?
