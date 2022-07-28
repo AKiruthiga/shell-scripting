1215e5e928c5c57e0abc96a11d390bbcd7140746
kiruthiga <kiruthiga.arasilangovan@gmail.com>
2022-07-28 19:52:32 +0530

    robocopy

--- Roboshop/components/mongodb.sh
+++ Roboshop/components/mongodb.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 
-echo "Hello, I am mongodb"
+set -e
+COMPONENT=MONGODB
+LOGFILE="/tmp/$COMPONENT.log"
+MONGODB_REPO="https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo"
+
+source components/common.sh
+
+echo -n "downloading the mongodb repo:"
+
+curl -s -o /etc/yum.repos.d/mongodb.repo $MONGODB_REPO
+stat $?
+
+echo -n "installing mongodb:"
+yum install -y mongodb-org &>> $LOGFILE
+stat $?
+
+echo -n "starting $COMPONENT"
+systemctl enable mongod &>> $LOGFILE
+systemctl start mongod &>> $LOGFILE
+stat $?
+
+# vim /etc/mongod.conf
+# systemctl restart mongod
+# curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+
+# cd /tmp
+# unzip mongodb.zip
+# cd mongodb-main
+# mongo < catalogue.js
+# mongo < users.js
