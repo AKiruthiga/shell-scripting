afe9f7850186fd4d83b345a10c8e6c2d71944ef9
kiruthiga <kiruthiga.arasilangovan@gmail.com>
2022-08-01 14:27:03 +0530

    robocopy

--- Roboshop/components/catalogue.sh
+++ Roboshop/components/catalogue.sh
@@ -1,3 +1,47 @@
 #!/bin/bash
 
-echo "Hello, I am catalogue"
+set -e
+COMPONENT=catalogue
+LOGFILE="/tmp/$COMPONENT.log"
+
+source components/common.sh
+
+echo -n "configuring Nodejs repo:"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> $LOGFILE
+stat $?
+
+echo -n "installing Nodejs repo:"
+yum install nodejs -y &>> $LOGFILE
+stat $?
+
+
+echo -n "creating the roboshop user:"
+id roboshop &>> $LOGFILE || useradd roboshop 
+stat $?
+
+echo -n "downloading $COMPONENT repo:"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip" &>> $LOGFILE
+cd /home/roboshop
+unzip /tmp/catalogue.zip &>> $LOGFILE
+mv catalogue-main catalogue
+cd /home/roboshop/catalogue
+
+echo -n "installing $COMPONENT:"
+npm install &>> $LOGFILE
+stat $?
+
+#vim systemd.servce
+# # mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+# # systemctl daemon-reload
+# # systemctl start catalogue
+# # systemctl enable catalogue
+# # systemctl status catalogue -l
+
+# NOTE: You should see the log saying `connected to MongoDB`, then only your catalogue
+# will work and can fetch the items from MongoDB
+
+# Ref Log:
+# {"level":"info","time":1656660782066,"pid":12217,"hostname":"ip-172-31-13-123.ec2.internal","msg":"MongoDB connected","v":1}
+
+# # vim /etc/nginx/default.d/roboshop.conf
+# # systemctl restart nginx
