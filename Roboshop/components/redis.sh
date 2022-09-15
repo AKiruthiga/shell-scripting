42db9682dea7bf9ccdf2553cf9a9ce64a13ed4a4
Kiruthiga <kiruthiga.arasilangovan@gmail.com>
2022-09-15 22:33:09 +0530

    my first prj

--- Roboshop/components/redis.sh
+++ Roboshop/components/redis.sh
@@ -11,9 +11,16 @@ echo -n "configuring the $COMPONENT repo:"
 curl -L https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo -o /etc/yum.repos.d/redis.repo &>> $LOGFILE
 stat $?
 
-echo -n "Installing $COMPONENT"
+echo -n "Installing $COMPONENT :"
 yum install redis-6.2.7 -y &>> $LOGFILE
 stat $?
+
+echo -n "Updating the BindIP:"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>> $LOGFILE
+stat $?
+
+#CALLING START SERVICE
+START_SERVICE
 
 #2. Update the BindIP from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf & /etc/redis/redis.conf
 
